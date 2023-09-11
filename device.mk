mejsij/lineage_device_nothing_Spacewar

Spacewar: Migrate to libperfmg power HAL AIDL
Change-Id: I42170d51a517170b58d532addd9c38496e43457c

device.mk
@@ -23,6 +23,8 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel \
     vendor/nxp/nfc \
     vendor/nxp/secure_element \
     vendor/qcom/opensource/usb/etc
@@ -337,7 +339,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
+    android.hardware.power-service.Spacewar-libperfmgr \
     android.hardware.power@1.3.vendor
 
 # PowerShare

nothing-sm7325/android_device_nothing_Spacewar

device.mk
@@ -24,6 +24,8 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/nothing \
+    hardware/google/interfaces \
+    hardware/google/pixel \
     vendor/nxp/nfc \
     vendor/nxp/secure_element \
     vendor/qcom/opensource/usb/etc
@@ -342,7 +344,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
+    android.hardware.power-service.nothing-libperfmgr \
     android.hardware.power@1.3.vendor
 
 # PowerShare